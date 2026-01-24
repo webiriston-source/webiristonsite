321aa6bf34ff5177da320086ca094019d24156fb
osdevirson <osdevirson@mail.ru>
2026-01-24 12:01:31 +0300

    fix: vite.config.ts syntax, vercel.json rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-            m.cartographer(),
-          ),
-            m.devBanner(),
-          ),
-        ]
-      : []),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
